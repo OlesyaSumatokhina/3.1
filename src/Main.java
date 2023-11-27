6315922eb5b4a4fa0007423960d4586ee30cc5a6
Olesya Sumatokhina <olesya_po@mail.ru>
2023-11-27 21:08:37 +0300

    newfile

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2020;
+        int bonus = 1;
+        int result = price / 20 * bonus;
+        System.out.println(result);
+    }
+}
